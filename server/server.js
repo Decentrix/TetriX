e5afe602c505597891c2716c83c532cf50d1fc8d
pskchang <pschang@ucsd.edu>
2018-04-25 14:22:22 -0700

    Moved client related stuff to client folder

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ const app = express();
 const PORT = 8080;
 
 app.use('/build', express.static(path.join(__dirname, '../build')));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/public/index.html')));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- src/parser.js
+++ src/parser.js
@@ -5,10 +5,11 @@ const words = source.trim().split('\n');
 //TODO: Test for cases where code has \n in string
 
 const saveDataType = (type, name, value) => {
+  const result = [];
+  for (let i = 0; i < words.length; i++) {
+    result.push(words[i]);
+  }
+  return result;
 };
-const result = [];
-for (let i = 0; i < words.length; i++) {
-  result.push(words[i]);
-}
 
-console.log(result);
+module.exports = saveDataType;
