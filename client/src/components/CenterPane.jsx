7b2d620017e79e408af22c81c3e084a4603620ef
Esther Kim <estherkim@MacBook-Air-2075.local>
2018-05-03 17:49:50 -0700

    merged changes of frontend and back end

--- client/src/components/CenterPane.jsx
+++ client/src/components/CenterPane.jsx
@@ -1,24 +1,12 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import OgCodeContainer from './OgCodeContainer.jsx';
+import OptCodeContainer from './OptCodeContainer.jsx';
 
 class CenterPane extends Component {
   render() {
-
     return (
       <div className="centerPiece">
-        <div>CHANGES MADE: </div>
-        <div className="lineContainer" id="changeOne">
-          <div className="lineNum">{4}</div>
-          <div className="code" style={{whiteSpace:'pre-wrap'}}>{'uint 8 a;'}</div>
-        </div>
-      <div>
-        
-
-      </div>
-        <div className="lineContainer" id="changeTwo">
-          <div className="lineNum">{7}</div>
-          <div className="code" style={{whiteSpace:'pre-wrap'}}>{'uint 8 a;'}</div>
-        </div>
+        This is the CenterPane
       </div>
     )
   }
