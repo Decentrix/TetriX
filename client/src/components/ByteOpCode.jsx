8efdbc77ee1e08ca540367eac656d8262344f107
Esther Kim <estherkim@MacBook-Air-2075.local>
2018-05-07 20:22:20 -0700

    fixed css for most frontend

--- client/src/components/ByteOpCode.jsx
+++ client/src/components/ByteOpCode.jsx
@@ -4,7 +4,7 @@ const ByteOpCode = (props) => {
   return (
     <div className="innerContainer">
       <div className="byteCode"><b>BYTECODE: </b>{props.info.bytecode}</div>
-      <div className="opCode"><b>OPCODE:</b>{props.info.opcodes}</div>
+      <div className="opCode"><b>OPCODE: </b>{props.info.opcodes}</div>
     </div>
   )
 }
