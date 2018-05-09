b3c5bb3bd1b0d6490e670d72b8ab4e83da8844dd
josephwu1994 <josephwu1994@gmail.com>
2018-05-08 20:59:08 -0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
         loader: "babel-loader",
         query: {
           presets: ["es2015", "react"]
