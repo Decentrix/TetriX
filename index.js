89110d1a1b14f1cdf8507b5077e8b5e7266144ee
josephwu1994 <josephwu1994@gmail.com>
2018-05-10 13:53:10 -0700

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const contract = require('./src/contract'); // Contract related methods
 const file = require('./src/file'); // Readies contract data for client
 const app = require('./src/app');
 const childProcess = require('child_process');
-if (process.argv[3] === '-v') childProcess.fork('node_modules/webpack/bin/webpack.js --config node_modules/tetrix/webpack.config.js');
+if (process.argv.includes('-v')) childProcess.fork('node_modules/webpack/bin/webpack.js --config');
 const contractPath = file.extractPath(process.argv);
 const oldSource = code.extractContent(contractPath);
 const newSource = opt.optimize(oldSource);
